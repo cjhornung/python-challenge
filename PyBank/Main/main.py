8b2dd73ea7d9233ba3687ab26f6d88899e7a50e4
cjhornung <cjhornung@gmail.com>
2023-06-24 20:04:05 -0500

    Fixed csvreader to output columns into lists
    Output columns into lists from the csv file for easier iteration of values. Added the dates for the greatest changes to the output. Only thing left is to reformat the dates per the desired module output

--- PyBank/Main/main.py
+++ PyBank/Main/main.py
@@ -13,29 +13,48 @@ greatest_increase=0
 greatest_increase_date=""
 greatest_decrease=0
 greatest_decrease_date=""
-
+changetotal=0
+date_list=[]
+profit_losses_list=[]
 # Open the CSV using the UTF-8 encoding
 with open(csvpath, encoding='UTF-8') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=",")
+        #Skip Headers
         next(csvreader, None)
+        #Place CSV data into lists
         for row in csvreader:
+                date_list.append(row[0])
+                profit_losses_list.append(row[1])
+        #Set up intial variable for comparison 
+        pastvalue=int(profit_losses_list[0])
+        #Loop to populate variables with total and change data
+        for i in range(len(profit_losses_list)):
+                currentvalue=int(profit_losses_list[i])
+                changetotal=currentvalue-pastvalue+changetotal
+                if currentvalue-pastvalue>greatest_increase:
+                       greatest_increase=currentvalue-pastvalue
+                       greatest_increase_date=date_list[i]
+                if currentvalue-pastvalue<greatest_decrease:
+                       greatest_decrease=currentvalue-pastvalue
+                       greatest_decrease_date=date_list[i]
                 total_months=total_months + 1
-                total=total+int(row[1])
-                             
+                total=total+int(profit_losses_list[i])
+                pastvalue=int(profit_losses_list[i])
+        average_change=round(changetotal/(total_months-1),2)                     
 
 
-
+#Terminal Output
 print("Financial Analysis")
 print("----------------------------")
 str_total_months="Total Months: "+str(total_months)
 str_total="Total: $"+str(total)
 str_average_change="Average Change: $"+str(average_change)
-str_greatest_increase="Greatest Increase in Profits: ($"+str(greatest_increase)+")"
-str_greatest_decrease="Greatest Decrease in Profits: ($"+str(greatest_decrease)+")"
+str_greatest_increase="Greatest Increase in Profits: "+greatest_increase_date+" ($"+str(greatest_increase)+")"
+str_greatest_decrease="Greatest Decrease in Profits: "+greatest_decrease_date+" ($"+str(greatest_decrease)+")"
 output=[str_total_months,str_total,str_average_change,str_greatest_increase,str_greatest_decrease]
 for line in range(len(output)):
     print(output[line])
-
+#Txt File Outupt
 output_file = os.path.join("..", "analysis","output.txt")
 with open(output_file, "w") as datafile:
     
--- PyBank/README.txt
+++ PyBank/README.txt
@@ -1,5 +1,6 @@
 How to write a string to a text file:
+Line 58-60
 https://www.pythontutorial.net/python-basics/python-write-text-file/
 How to skiop the first row in reading a csv file:
-Line 20
+Line 23
 https://stackoverflow.com/questions/14257373/how-to-skip-the-headers-when-processing-a-csv-file-using-python
